2b7b2fd541e7fbfefcb6a29871c10b1a88d1ba11
Dario Evers <darioevers@gmail.com>
2021-02-16 14:11:26 +0100

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,6 +1,32 @@
-// ASSIGNMENT 1:
-const sumOfNumbers = (arr) => {
-  let sum = arr.reduce((a, b) => a + b, 0);
-  return sum;
+// TERNARY OPERATOR:
+const numIsEqual = (num) => {
+  return num % 2 == 0
+    ? console.log(num + " is even.")
+    : console.log(num + " is odd.");
 };
-console.log(sumOfNumbers([1, 2, 3, 4]));
+numIsEqual(2);
+
+// IF ELSE:
+const numIsEqualTwo = (num) => {
+  if (num % 2 == 0) {
+    console.log(num + " is even.");
+  } else {
+    console.log(num + " is odd.");
+  }
+};
+numIsEqualTwo(4);
+
+// SWITCH:
+const numIsEqualThree = (num) => {
+  switch (num % 2 == 0) {
+    case true:
+      console.log(num + " is even.");
+      break;
+    case false:
+      console.log(num + " is odd.");
+      break;
+    default:
+      console.log("Something is wrong.");
+  }
+};
+numIsEqualThree(6);
