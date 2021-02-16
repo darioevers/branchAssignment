cfaf1bf143ff65701d1f45f41df4a334a133c7b8
Dario Evers <darioevers@gmail.com>
2021-02-16 13:48:33 +0100

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,6 +1,3 @@
-<<<<<<< Updated upstream
-1;
-=======
 // I cannot find assignments where I know about 3 different solving ways.
 
 // Assignment 1: Ternary Operator, If else, Switch
@@ -8,4 +5,3 @@
 // Assignment 3: Ternary Operator, If else, Switch
 
 ??????????????
->>>>>>> Stashed changes
