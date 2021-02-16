82334de134e8729e71e08ff2c21de154e734283b
Dario Evers <darioevers@gmail.com>
2021-02-16 13:49:10 +0100

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 // I cannot find assignments where I know about 3 different solving ways.
 
 // Assignment 1: Ternary Operator, If else, Switch
 // Assignment 2: Ternary Operator, If else, Switch
 // Assignment 3: Ternary Operator, If else, Switch
-
-??????????????
-=======
-1;
-3;
->>>>>>> a839a58d4c0a4feb9b6faa11188f66f927590931
