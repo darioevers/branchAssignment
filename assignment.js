fb4928a0805a7c4e74655f8ad4e9b2a242fd03b8
Dario Evers <darioevers@gmail.com>
2021-02-16 15:41:33 +0100

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,1 +1,30 @@
-///
+/// IF ELSE .INCLUDES
+const checkStr = (str) => {
+  if (str.includes("JavaScript")) {
+    console.log("I know it's crazy 😉 NR1");
+  } else {
+    console.log(str);
+  }
+};
+checkStr("I like JavaScript.");
+
+/// SWITCH
+const checkStrTwo = (str) => {
+  switch (str.includes("JavaScript")) {
+    case true:
+      console.log("I know it's crazy 😉 NR2");
+      break;
+    case false:
+      console.log(str);
+      break;
+  }
+};
+checkStrTwo("I like JavaScript.");
+
+// TERNARY
+const checkStrThree = (str) => {
+  return str.includes("JavaScript")
+    ? console.log("I know it's crazy 😉 NR3")
+    : console.log(str);
+};
+checkStrThree("I like JavaScript.");
