10e898073f63d77b39ebdcfae36618323ea2dbd9
Dario Evers <darioevers@gmail.com>
2021-02-16 13:51:10 +0100

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,5 +1,17 @@
 // I cannot find assignments where I know about 3 different solving ways.
 
-// Assignment 1: Ternary Operator, If else, Switch
+// Assignment 1: Loop, .reduce, ?????
 // Assignment 2: Ternary Operator, If else, Switch
 // Assignment 3: Ternary Operator, If else, Switch
+
+// ASSIGNMENT 1:
+const sumOfNumbers = (arr) => {
+  var product = 1;
+  var sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    product *= arr[i];
+  }
+  return `The sum of adding up ${arr} is ${sum}, and the product of multiplying ${arr} is ${product}.`;
+};
+console.log(sumOfNumbers([1, 2, 3, 4]));
