b8321fac1ed359c4539bfd41b05388fe2a65f5f0
Dario Evers <darioevers@gmail.com>
2021-02-16 14:11:58 +0100

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,11 +1,1 @@
-// ASSIGNMENT 1:
-const sumOfNumbers = (arr) => {
-  if (!Array.isArray(arr)) return;
-  let totalNumber = 0,
-    i = -1;
-  while (++i < arr.length) {
-    totalNumber += arr[i];
-  }
-  return totalNumber;
-};
-console.log(sumOfNumbers([1, 2, 3, 4]));
+///
